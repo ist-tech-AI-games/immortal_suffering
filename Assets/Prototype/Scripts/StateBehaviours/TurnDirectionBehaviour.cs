933a505662ba0087638b13820663cca7476bbd2f
km20511 <fallsky1121@daum.net>
2025-08-07 16:25:18 +0900

    [feat][res] Apply sprites for enemies and levels. Implement taxi distance measurement.

--- Assets/Prototype/Scripts/StateBehaviours/TurnDirectionBehaviour.cs
+++ Assets/Prototype/Scripts/StateBehaviours/TurnDirectionBehaviour.cs
@@ -6,11 +6,12 @@ namespace ImmortalSuffering.StateBehaviours
     {
         [System.Flags] enum DirMode { Param = 1, Context = 2 }
         enum SrcDirMode { ToggleFromParam, ToggleFromContext }
+        enum RotationControlMode { DontUseRotation, Animate, SetOnEnter, SetOnExit }
         // enum SrcDirMode { LookAtTarget, ToggleFromParam, ToggleFromContext }
         [SerializeField] private SrcDirMode srcDirMode;
         [SerializeField] private DirMode dstDirMode;
         [SerializeField] private string paramName;
-        [SerializeField] private bool animateRotation = true;
+        [SerializeField] private RotationControlMode rotationControlMode = RotationControlMode.Animate;
         [SerializeField] private float rightRotation = 180f;
         [SerializeField] private float leftRotation = 0f;
         private EnemyFSMContext context;
@@ -50,19 +51,23 @@ namespace ImmortalSuffering.StateBehaviours
             if ((dstDirMode & DirMode.Context) != 0)
                 context.FacingRight = facingRight;
             fromTo = facingRight ? new Vector2(leftRotation, rightRotation) : new Vector2(rightRotation, leftRotation);
+
+            if (rotationControlMode == RotationControlMode.SetOnEnter)
+                SetAngle(animator, fromTo.y);
         }
 
         public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            if (!animateRotation) return; // || !shouldTurn) return;
-
-            animator.transform.rotation = Quaternion.AngleAxis(Mathf.LerpAngle(fromTo.x, fromTo.y, stateInfo.normalizedTime), Vector3.up);
+            if (rotationControlMode == RotationControlMode.Animate)
+                SetAngle(animator, Mathf.LerpAngle(fromTo.x, fromTo.y, stateInfo.normalizedTime));
         }
 
         public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            if (animateRotation)
-                animator.transform.rotation = Quaternion.AngleAxis(fromTo.y, Vector3.up); //shouldTurn ? fromTo.y : (facingRight ? 0f : 180f), Vector3.up);
+            if (rotationControlMode == RotationControlMode.Animate || rotationControlMode == RotationControlMode.SetOnExit)
+                SetAngle(animator, fromTo.y);
         }
+
+        private void SetAngle(Animator animator, float angle) { animator.transform.rotation = Quaternion.AngleAxis(angle, Vector3.up); }
     }
 }
