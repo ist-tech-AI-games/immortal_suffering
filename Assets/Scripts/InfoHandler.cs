76f61e2f180a593be9727d2ba5bdc33ddd37b59e
km20511 <fallsky1121@daum.net>
2025-08-10 15:08:55 +0900

    [fix] Split alpha channel from the compare process.

--- Assets/Scripts/InfoHandler.cs
+++ Assets/Scripts/InfoHandler.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -21,17 +23,19 @@ public class InfoHandler : MonoBehaviour
     [SerializeField] private RenderTexture entireScreenTexture; // 전체 화면 텍스처
 
     [Serializable]
-    public class ColorMap
+    public struct ColorMap
     {
         public int Id;
         public Color Color;
     }
+    const int COLORMAPSTRUCTSIZE = sizeof(int) + 4 * sizeof(float);
 
     [Tooltip("Mapping of color - id. Warning: max 16 entries.")]
     [SerializeField] private ColorMap[] colorMaps;
     [SerializeField] private ColorMap fallbackMap;
-    [SerializeField] private Vector2Int sampleSize = new (160, 90);
+    [SerializeField] private Vector2Int sampleSize = new(160, 90);
     [SerializeField, Range(0f, 1f)] private float compareThreshold = .2f;
+    [SerializeField, Range(0f, 1f)] private float alphaThreshold = .5f;
     [Header("Outputs - You can modify these for debugging")]
     [SerializeField] private RawImage rawImage;
 
@@ -45,10 +49,12 @@ public class InfoHandler : MonoBehaviour
         _DebugTexID = Shader.PropertyToID("DebugTex"),
         _SourceTexID = Shader.PropertyToID("SourceTex"),
         _ColorCountID = Shader.PropertyToID("ColorCount"),
-        _ColorIDsID = Shader.PropertyToID("ColorIDs"),
+        _ColorMapID = Shader.PropertyToID("ColorMap"),
+        // _ColorIDsID = Shader.PropertyToID("ColorIDs"),
         _FallbackIDID = Shader.PropertyToID("FallbackColorID"),
         _CompareThresholdID = Shader.PropertyToID("CompareThreshold"),
-        _ColorValuesID = Shader.PropertyToID("ColorValues"),
+        _AlphaThresholdID = Shader.PropertyToID("AlphaThreshold"),
+        // _ColorValuesID = Shader.PropertyToID("ColorValues"),
         _FallbackColorID = Shader.PropertyToID("FallbackColor"),
         _WidthID = Shader.PropertyToID("Width"),
         _HeightID = Shader.PropertyToID("Height");
@@ -68,8 +74,11 @@ public class InfoHandler : MonoBehaviour
 
     public void InitMinimapMapping()
     {
-        debugTexture = new RenderTexture(sampleSize.x, sampleSize.y, 1);
-        debugTexture.enableRandomWrite = true;
+        var desc = entireScreenTexture.descriptor;
+        desc.width = sampleSize.x; desc.height = sampleSize.y;
+        desc.enableRandomWrite = true;
+        debugTexture = new RenderTexture(desc);
+
         if (rawImage != null) rawImage.texture = debugTexture;
         result = new int[sampleSize.x * sampleSize.y];
     }
@@ -92,26 +101,31 @@ public class InfoHandler : MonoBehaviour
         int kernel = computeShader.FindKernel("CSMain");
         ComputeBuffer resultCB = new(pixelCnt, sizeof(int));
 
-        int[] ids = new int[maxMapping];
-        Vector4[] colors = new Vector4[maxMapping];
+        ComputeBuffer colorMapCB = new(colorMaps.Length, COLORMAPSTRUCTSIZE);
+        colorMapCB.SetData(colorMaps);
 
-        for (int i = 0; i < colorMaps.Length; i++)
-        {
-            ids[i] = colorMaps[i].Id;
-            colors[i] = colorMaps[i].Color;
-        }
+        // int[] ids = new int[maxMapping];
+        // Vector4[] colors = new Vector4[maxMapping];
+
+        // for (int i = 0; i < colorMaps.Length; i++)
+        // {
+        //     ids[i] = colorMaps[i].Id;
+        //     colors[i] = colorMaps[i].Color;
+        // }
 
         computeShader.SetTexture(kernel, _SourceTexID, entireScreenTexture);
         computeShader.SetTexture(kernel, _DebugTexID, debugTexture);
         computeShader.SetBuffer(kernel, _ResultID, resultCB);
+        computeShader.SetBuffer(kernel, _ColorMapID, colorMapCB);
         computeShader.SetInt(_WidthID, sampleSize.x);
         computeShader.SetInt(_HeightID, sampleSize.y);
         computeShader.SetInt(_ColorCountID, colorMaps.Length);
         computeShader.SetInt(_FallbackIDID, fallbackMap.Id);
         computeShader.SetFloat(_CompareThresholdID, compareThreshold);
-        computeShader.SetInts(_ColorIDsID, ids);
+        computeShader.SetFloat(_AlphaThresholdID, alphaThreshold);
+        // computeShader.SetInts(_ColorIDsID, ids);
         computeShader.SetVector(_FallbackColorID, fallbackMap.Color);
-        computeShader.SetVectorArray(_ColorValuesID, colors);
+        // computeShader.SetVectorArray(_ColorValuesID, colors);
 
         Vector2Int threadGroups = Vector2Int.CeilToInt(new Vector2(entireScreenTexture.width, entireScreenTexture.height) / 8f);
         computeShader.Dispatch(kernel, threadGroups.x, threadGroups.y, 1);
@@ -120,6 +134,21 @@ public class InfoHandler : MonoBehaviour
         resultCB.Release();
 
         return true;
+    }
+
+    // --debug--
+    [ContextMenu("Debug: Write to file")]
+    public void WriteResultToFile()
+    {
+        StringBuilder stringBuilder = new();
+        for (int i = 0; i < sampleSize.y; i++)
+        {
+            for (int j = 0; j < sampleSize.x; j++)
+                stringBuilder.AppendFormat("{0:00} ", result[i * sampleSize.x + j]);
+            stringBuilder.Append("\n");
+        }
+
+        File.WriteAllText(Path.Combine(Application.dataPath, "ColorIds.txt"), stringBuilder.ToString());
     }
 
 
